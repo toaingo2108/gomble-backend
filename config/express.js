6622462aab3aa2bfc3bf5fb5321f1c63e62ea519
Haley Huynh <haleyhtnt1122@gmail.com>
2020-09-01 03:13:39 -0700

    Configuration

--- config/express.js
+++ config/express.js
@@ -35,25 +35,30 @@ app.use(helmet());
 app.use(cors());
 
 // enable detailed API logging in dev env
-if (config.env === 'development') {
-  expressWinston.requestWhitelist.push('body');
-  expressWinston.responseWhitelist.push('body');
-  app.use(expressWinston.logger({
-    winstonInstance,
-    meta: true, // optional: log meta data about request (defaults to true)
-    msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms',
-    colorStatus: true // Color the status code (default green, 3XX cyan, 4XX yellow, 5XX red).
-  }));
-}
+// if (config.env === "development") {
+//   expressWinston.requestWhitelist.push("body");
+//   expressWinston.responseWhitelist.push("body");
+//   app.use(
+//     expressWinston.logger({
+//       winstonInstance,
+//       meta: true, // optional: log meta data about request (defaults to true)
+//       msg:
+//         "HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms",
+//       colorStatus: true, // Color the status code (default green, 3XX cyan, 4XX yellow, 5XX red).
+//     })
+//   );
+// }
 
 // mount all routes on /api path
-app.use('/api', routes);
+app.use("/api", routes);
 
 // if error is not an instanceOf APIError, convert it.
 app.use((err, req, res, next) => {
   if (err instanceof expressValidation.ValidationError) {
     // validation error contains errors which is an array of error each containing message[]
-    const unifiedErrorMessage = err.errors.map(error => error.messages.join('. ')).join(' and ');
+    const unifiedErrorMessage = err.errors
+      .map((error) => error.messages.join(". "))
+      .join(" and ");
     const error = new APIError(unifiedErrorMessage, err.status, true);
     return next(error);
   } else if (!(err instanceof APIError)) {
@@ -65,22 +70,29 @@ app.use((err, req, res, next) => {
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new APIError('API not found', httpStatus.NOT_FOUND);
+  const err = new APIError("API not found", httpStatus.NOT_FOUND);
   return next(err);
 });
 
 // log error in winston transports except when executing test suite
-if (config.env !== 'test') {
-  app.use(expressWinston.errorLogger({
-    winstonInstance
-  }));
+if (config.env !== "test") {
+  app.use(
+    expressWinston.errorLogger({
+      winstonInstance,
+    })
+  );
 }
 
 // error handler, send stacktrace only during development
-app.use((err, req, res, next) => // eslint-disable-line no-unused-vars
+app.use((
+  err,
+  req,
+  res,
+  next // eslint-disable-line no-unused-vars
+) =>
   res.status(err.status).json({
     message: err.isPublic ? err.message : httpStatus[err.status],
-    stack: config.env === 'development' ? err.stack : {}
+    stack: config.env === "development" ? err.stack : {},
   })
 );
 
