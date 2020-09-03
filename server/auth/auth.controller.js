660c915d060ec70bd928c435f4bb4fa473bf97f5
Haley Huynh <haleyhtnt1122@gmail.com>
2020-09-03 09:18:18 -0700

    fblogin

--- server/auth/auth.controller.js
+++ server/auth/auth.controller.js
@@ -5,13 +5,6 @@ const config = require("../../config/config");
 const User = require("../user/user.model");
 const bcrypt = require("bcryptjs");
 
-/**
- * Returns jwt token if valid username and password is provided
- * @param req
- * @param res
- * @param next
- * @returns {*}
- */
 async function login(req, res) {
   const email = req.body.email;
   const password = req.body.password;
@@ -105,18 +98,75 @@ async function register(req, res) {
   }
 }
 
-/**
- * This is a protected route. Will return random number only if jwt token is provided in header.
- * @param req
- * @param res
- * @returns {*}
- */
-function getRandomNumber(req, res) {
-  // req.user is assigned by jwt middleware if valid token is provided
-  return res.json({
-    user: req.user,
-    num: Math.random() * 100,
-  });
+async function fblogin(req, res) {
+  const email = req.body.email;
+  const accessToken = req.body.accessToken;
+  if (!email) {
+    return res.status(400).json({
+      success: false,
+      message: "Email is required",
+    });
+  }
+  if (!accessToken) {
+    return res.status(400).json({
+      success: false,
+      message: "accessToken is required",
+    });
+  }
+
+  // if (req.body.name == undefined) {
+  //   return res.status(201).json({ success: false, msg: "name is missing" });
+  // }
+  // if (req.body.id == undefined) {
+  //   return res.status(201).json({ success: false, msg: "id is missing" });
+  // }
+
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      if (!user.is_facebook) {
+        return res
+          .status(201)
+          .json({ success: false, email: "Email already exists" });
+      }
+      const payload = {
+        _id: user.id,
+        email: user.email,
+        is_facebook: true,
+      };
+      let token = jwt.sign(payload, config.jwtSecret, {
+        expiresIn: 31556926, // 1 year in seconds
+      });
+      return res.status(200).json({
+        success: true,
+        token: "Bearer " + token,
+        type: user.type,
+      });
+    } else {
+      const newUser = new User({
+        email,
+        password: bcrypt.hashSync(req.body.accessToken, 10),
+        is_facebook: true,
+      });
+
+      await newUser.save();
+      const payload = {
+        _id: newUser._id,
+        email: email,
+        is_facebook: true,
+      };
+      let token = jwt.sign(payload, config.jwtSecret, {
+        expiresIn: 31556926, // 1 year in seconds
+      });
+      return res.status(200).json({
+        success: true,
+        token: "Bearer " + token,
+        type: "-",
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({ success: false, message: `err: ${err}` });
+  }
 }
 
-module.exports = { login, getRandomNumber, register };
+module.exports = { login, register, fblogin };
